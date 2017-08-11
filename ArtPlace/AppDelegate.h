4711f50c758a2770930aeeef761feb3af24d49df
Mikhail Yaskou <mikhailyaskow@gmail.com>
2017-08-11 11:37:11 +0300

    Initial Commit

---
+++ ArtPlace/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ArtPlace
+//
+//  Created by Mikhail Yaskou on 11.08.17.
+//  Copyright © 2017 Mikhail Yaskou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ArtPlace/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ArtPlace
+//
+//  Created by Mikhail Yaskou on 11.08.17.
+//  Copyright © 2017 Mikhail Yaskou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
